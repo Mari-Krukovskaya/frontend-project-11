44b6a1c305e14b1f4b53ca2dbe1e48155c1b9fa7
Mari Kruk <mari.krukovskaya.97@mail.ru>
2023-08-17 16:35:01 +0300

    modalWindow

--- src/app.js
+++ src/app.js
@@ -9,20 +9,45 @@ import resources from './locales/index.js';
 
 const timeInterval = 5000;
 const timeOut = 1000;
+const defaultLanguage = 'ru';
 
 const validation = (url, listUrls) => {
   const schema = yup.string().url().required().notOneOf(listUrls);
     return schema.validate(url)
 };
+
+const formStatusError = (error) => {
+  const errorTypes = {
+    AxiosEroor: 'connectionError',
+    ParserError: 'invalidFeed',
+  };
+  return errorTypes[error.name] || 'defaultError';
+};
+
+
 const fetchRemoteContent = (url) => {
- return new Promise((resolve, reject) => {
-  axios({
+watchedState.LoadingFeedback = { formStatus: 'loading', error: null };
+
+ return axios({
     method: 'get',
     url: `https://allorigins.hexlet.app/get?disableCache=true&url=${url}`,
    time: timeOut,
   })
-  .then((responce) => resolve(responce))
-  .catch((error) => reject(error));
+  .then((responce) => {
+    const { feed, posts } = parseFeedData(responce.data.contents);
+    feed.url = url;
+    feed.id = _.uniqueId();
+
+    posts.forEach((post) => {
+      post.id = _.uniqueId();
+      post.feedId = feed.id;
+    });
+    watchedState.LoadingFeedback = {formStatus: 'success', error: null };
+    watchedState.feeds.push(feed);
+    watchedState.posts.push(...posts);
+ })
+ .catch((error) => {
+  watchedState.LoadingFeedback = { formStatus: 'failed', error: formStatusError(error)}
  });
 };
 
@@ -58,7 +83,6 @@ const checkNewPosts = (state) => {
 
 
 export default () => {
-  const defaultLanguage = 'ru';
   const i18nInstance = i18next.createInstance();
   i18nInstance.init({
 lng: defaultLanguage,
@@ -66,6 +90,7 @@ debug: false,
 resources,
   })
   .then(() => {
+ 
     yup.setLocale({
       mixed: {
           notOneOf: 'rssAlreadyExists',
@@ -74,50 +99,89 @@ resources,
       string: {
           url: 'invalidUrl',
       },
-      });
+    });
 
-      const elements = {
-        form: document.querySelector('.rss-form'),
-        input: document.querySelector('input[name="url"]'),
-        submitBtn: document.querySelector('button[type="submit"]'),
-        feedback: document.querySelector('.feedback'),
-        posts: document.querySelector('.posts'),
-        feeds: document.querySelector('.feeds'),
-        modal: {
-          modalWindow: document.querySelector('.modal'),
-          title: document.querySelector('.modal-title'),
-          body: document.querySelector('.modal-body'),
-          button: document.querySelector('.full-article'),
-        },
-    };
-    const state = {
-      form: {
-        feedUrl: [],
-        isFeedValid: false,
-        error: null,
-      },
-        feeds: [],
-        posts: [],
+  const elements = {
+    form: document.querySelector('.rss-form'),
+    input: document.querySelector('input[name="url"]'),
+    submitBtn: document.querySelector('button[type="submit"]'),
+    feedback: document.querySelector('.feedback'),
+    posts: document.querySelector('.posts'),
+    feeds: document.querySelector('.feeds'),
+    modal: {
+      modalWindow: document.querySelector('.modal'),
+      title: document.querySelector('.modal-title'),
+      body: document.querySelector('.modal-body'),
+      button: document.querySelector('.full-article'),
+    },
+};
 
-      postViewState: {
-        currentPostId: null,
-        visitedPostsId: new Set(),
-      },
+const state = {
+  form: {
+    feedUrl: [],
+    isFeedValid: false,
+    error: null,
+  },
+    feeds: [],
+    posts: [],
 
-      LoadingFeedback: {
-        formStatus: 'filling',
-        error: null,
-      },
-      };
-const watchedState = render(state, elements, i18nInstance)      
+  postViewState: {
+    currentPostId: null,
+    visitedPostsId: new Set(),
+  },
+
+  LoadingFeedback: {
+    formStatus: 'filling',
+    error: null,
+  },
+  };
+
+      const watchedState = render(state, elements, i18nInstance)      
+      // checkNewPosts(watchedState);
+  
+elements.form.addEventListener('input', (even) => {
+ even.preventDefault();
+ watchedState.LoadingFeedback.formStatus = 'loading';
+ watchedState.form.inputUrl = e.target.value;
+});
 
 elements.form.addEventListener('submit', (even) => {
 even.preventDefault();
   const formData = new FormData(e.target);
-  const url = formData.get('url');
+  const urls = formData.get('url');
+   const links = watchedState.feeds.map(({ link }) => link);
 
-  const links = watchedState.feeds.map((feed) => feed.url);
-  });
+  validation(urls, links)
+  .then((error) => {
+    if (error) {
+      watchedState.form = { isFeedValid: false, error: error.message };
+      return;
+    }
+watchedState.form = { isFeedValid: true, error: null };
+fetchRemoteContent(urls, watchedState) 
+   });
+ });
+elements.modal.modalWindow.addEventListener('how.bs.modal', (even) => {
+  const postId = even.relatedTarget.getAttribute('data-id');
+  watchedState.postViewState.visitedPostsId.push(postId);
+  watchedState.postViewState.currentPostId = postId;
 });
 
-};
+  elements.posts.addEventListener('click', (even) => {
+    const { id } = even.target.dataset;
+    if (!id) {
+      return;
+    }
+    watchedState.postViewState.currentPostId = id;
+    watchedState.postViewState.visitedPostsId.add(id);
+  });
+});
+  }
+
+
+
+
+
+
+
+
--- src/view.js
+++ src/view.js
@@ -15,25 +15,42 @@ const renderLoadingFeedback = (value, elements, i18nInstance) => {
   const  { form, input, submit, feedback } = elements;
 
   input.removeAttribute('readonly');
-  submit.removeAttribute('desabled');
+  submit.removeAttribute('disabled');
   input.classList.remove('is-invalid');
-  feedback.classList.remove('text-success', 'text-danger');
+  feedback.classList.remove('text-danger');
+  feedback.classList.remove('text-success');
 
-  switch(formStatus) {
+  switch (formStatus) {
     case 'loading':
       input.setAttribute('readonly', 'readonly');
       submit.setAttribute('disabled', 'disabled');
+      input.classList.remove('is-invalid');
+      feedback.classList.remove('text-danger');
+      feedback.classList.remove('text-success');
+      feedback.textContent = i18nInstance.t('loading');
+      break;
+
+    case 'success':
+      input.removeAttribute('readonly');
+      submit.removeAttribute('disabled');
+      feedback.classList.remove('text-danger');
+      feedback.classList.add('text-success');
       feedback.textContent = i18nInstance.t('success');
       form.reset();
       input.focus();
       break;
+
     case 'failed':
+      input.removeAttribute('readonly');
+      submit.removeAttribute('disabled');
       input.classList.add('is-invalid');
+      feedback.classList.remove('text-success');
       feedback.classList.add('text-danger');
       feedback.textContent = i18nInstance.t(`error.${error}`);
       break;
-      default:
-        break;
+
+    default:
+      break;
   }
 };
 const renderPosts = (state, div, i18nInstance) => {
